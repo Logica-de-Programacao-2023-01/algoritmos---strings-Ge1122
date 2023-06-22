3ce62f60a549a12d3e335c3156543f9b07cbe5f9
Ge1122 <104367200+Ge1122@users.noreply.github.com>
2023-06-22 14:56:41 -0300

    solução13.go

--- solução13.go
+++ solução13.go
@@ -1,1 +1,35 @@
 
+package main
+
+
+import (
+	"fmt"
+	"strconv"
+)
+func main() {
+	var x string
+	fmt.Print("Digite uma sequência numérica: ")
+	fmt.Scan(&x)
+  
+	previonum := -1
+	isIncreasing := true
+  
+	for _, char := range x {
+		num, err := strconv.Atoi(string(char))
+		if err != nil {
+			fmt.Println("Entrada inválida.")
+			return
+		}
+		if num <= previonum {
+			isIncreasing = false
+			break
+		}
+		previonum = num
+	}
+
+	if isIncreasing {
+		fmt.Println("A sequência é numérica crescente.")
+	} else {
+		fmt.Println("A sequência não é numérica crescente.")
+	}
+}
